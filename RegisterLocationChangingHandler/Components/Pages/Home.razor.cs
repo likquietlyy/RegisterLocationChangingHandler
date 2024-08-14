e8eb85111faf303950eb53e30f9eb6eb92acb47f
kirill.reut <kirill.reut@BB12>
2024-08-14 09:52:53 +0300

    Add project files.

---
+++ RegisterLocationChangingHandler/Components/Pages/Home.razor.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Components.Routing;
+using Microsoft.AspNetCore.Components;
+
+namespace RegisterLocationChangingHandler.Components.Pages;
+
+public partial class Home : IDisposable
+{
+  [Inject] NavigationManager NavigationManager { get; init; } = default!;
+
+  private IDisposable? _registration;
+
+  protected override void OnAfterRender(bool firstRender)
+  {
+    if (firstRender)
+    {
+      _registration = NavigationManager.RegisterLocationChangingHandler(OnBeforeLocationChanging);
+    }
+  }
+
+  private ValueTask OnBeforeLocationChanging(LocationChangingContext context)
+  {
+    //some code
+    return ValueTask.CompletedTask;
+  }
+
+  public void Dispose()
+  {
+    _registration?.Dispose();
+  }
+}
